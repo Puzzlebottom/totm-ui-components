8807ee0320c84614a70717a84d1b9953d70cf117
Conor Meldrum <conor.j.meldrum@gmail.com>
2025-10-21 10:54:44 -0400

    Initialize expo-app and add storybook
    Generated by create-expo-app 3.5.3.

---
+++ components-dev-testing/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Protected guard={__DEV__}>
+        <Stack.Screen name="storybook" />
+      </Stack.Protected>
+    </Stack>
+  );
+}
