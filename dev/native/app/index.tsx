50e540d501019551f553a5041f21c9dbc0bcf4b2
Conor Meldrum <conor.j.meldrum@gmail.com>
2025-10-22 21:49:27 -0400

    initial commit

---
+++ dev/native/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text } from "react-native";
+import { View } from "components/view";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
---
+++ dev/web/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { View } from "components/view";
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+      }
+    >
+      <p>
+        Get started by editing & nbsp;
+        <code className="font-mono font-bold" > app / page.tsx </code>
+      </p>
+    </View>
+  );
+}
