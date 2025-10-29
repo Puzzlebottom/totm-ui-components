88226998722f49f03df0180cab9b99125e21bcd8
Conor Meldrum <conor.j.meldrum@gmail.com>
2025-10-29 12:14:04 -0400

    remove build folder from git

--- src/dist/components/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Button, ButtonContext, ButtonFrame, ButtonText } from './button';
-export type { ButtonProps } from './button';
--- src/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as config } from './tamagui/config';
-export { UIProvider } from './providers';
-export * from './components';
